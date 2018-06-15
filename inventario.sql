188e1e9265551975e0420c4d50f6f008f5f1d472
Luis Tapia <36052812+14030598@users.noreply.github.com>
2018-06-15 01:01:16 -0500

    haciendo script
    Co-Authored-By: Nair <14030637@itcelaya.edu.mx>

--- inventario.sql
+++ inventario.sql
@@ -3,18 +3,15 @@ create database inventario;
 use inventario;
 
 create table productos(codigo varchar(5),
+					nombre varchar(30),
+					descripcion varchar (100),
 					precio_compra money,
-					--... siguele
+					precio_venta money,
+					existencia int,
+					constraint primary key
 
+create table clientes();
 
+create table apartados();
 
-
-
-
-
-
-
-
-
-
-
+create table ventas();
