a5b6be6345b3fff6475907699df85c48c91d3028
Luis Tapia <36052812+14030598@users.noreply.github.com>
2018-06-15 01:02:27 -0500

    modificando script
    Co-Authored-By: Nair <14030637@itcelaya.edu.mx>

--- inventario.sql
+++ inventario.sql
@@ -2,16 +2,50 @@ create database inventario;
 
 use inventario;
 
-create table productos(codigo varchar(5),
-					nombre varchar(30),
-					descripcion varchar (100),
-					precio_compra money,
-					precio_venta money,
-					existencia int,
-					constraint primary key
+create table productos(codigo varchar(5) not null,
+					             nombre varchar(30),
+					             descripcion varchar (100),
+										 	 precio_compra money,
+					             precio_venta money,
+											 existencia int,
+					             constraint PRODUCTOSPK primary key (codigo));
 
-create table clientes();
+create table clientes(nombre_cliente varchar (50) not null,
+											constraint CLIENTESPK primary key (nombre));
 
-create table apartados();
+create table apartados(codigo varchar(5),
+										   nombre_cliente varchar (50),
+										   fecha_apartado date,
+                       fecha_liquidacion date,
+									     precio money,
+									     importe money,
+									     constraint APARTADOSPK primary key (codigo,nombre_cliente)
+										   constraint APARTADOSFK1 foreign key (codigo) references productos (codigo)
+										   constraint APARTADOSFK2 foreign key (nombre_cliente) references clientes (nombre_cliente));
 
-create table ventas();
+create table venta_es_de_producto(no_ticket int,
+																	codigo varchar(5));
+
+create table ventas(codigo varchar (5),
+										cantidad int,
+										fecha_venta date,
+										precio_venta money,
+									  total_a_pagar money,
+                    constraint VENTASPK primary key (codigo));
+
+
+
+
+
+
+
+
+
+
+
+create trigger apartando
+							on apartados for insert
+as
+begin
+			--si se inserta un nuevo apartado se tiene que quitar la existencia de productos en la cantidad apartada
+end
